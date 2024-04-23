250ff2fa3517989f36e9cf0a6451cc12671ffa2c
Benjamin Toro <benjamintoroj@gmail.com>
2024-04-23 22:34:07 +0700

    update

--- pkg/database/db.go
+++ pkg/database/db.go
@@ -2,15 +2,17 @@ package database
 
 import (
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func Start_db() *gorm.DB {
+	dsn := os.Getenv("DSN")
 	db, err := gorm.Open(postgres.New(postgres.Config{
-		DSN:                  "host=localhost user=postgres password=mysecretpassword dbname=postgres port=5430 sslmode=disable",
-		PreferSimpleProtocol: true, // disables implicit prepared statement usage
+		DSN:                  dsn,
+		PreferSimpleProtocol: true,
 	}), &gorm.Config{})
 
 	if err != nil {
@@ -22,17 +24,6 @@ func Start_db() *gorm.DB {
 	if err := db.AutoMigrate(&User{}, &Device{}, &Space{}, &Whitelist{}, &Log{}, &Role{}); err != nil {
 		log.Fatal("Failed migration:", err)
 	}
-
-	// Create roles
-	// roles := []Role{
-	// 	{ID: 1, Name: "admin", Description: "Administrator"},
-	// 	{ID: 2, Name: "verified", Description: "Verified User"},
-	// 	{ID: 3, Name: "unverified", Description: "Unverified User"},
-	// }
-
-	// // Create roles
-	// db.Create(&roles)
-	// Add table suffix when creating tables
 
 	db.Set("gorm:table_options", "ENGINE=InnoDB").AutoMigrate(&User{})
 
